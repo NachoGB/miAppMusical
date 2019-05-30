740f809db30744bef2f872655227299c95012e01
NachoGB <NGomez12345@gmail.com>
2019-05-30 21:54:54 +0200

    Funciona

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  instrumentos = [
+    {
+        nombre: 'flauta',
+        imagen: 'assets/imagenes/flauta.png',
+        audio: 'assets/sonidos/flauta.mp3'
+    },
+    {
+        nombre: 'bombo1',
+        imagen: 'assets/imagenes/bombo1.png',
+        audio: 'assets/sonidos/bombo1.mp3'
+    },
+    {
+        nombre: 'bombo2',
+        imagen: 'assets/imagenes/bombo2.png',
+        audio: 'assets/sonidos/bombo2.mp3'
+    },
+    {
+        nombre: 'platillos',
+        imagen: 'assets/imagenes/platillos.png',
+        audio: 'assets/sonidos/platillos.mp3'
+    }
+  ]
+  
+  reproducirSonido(instrumento){
+    let sonido = new Audio();
+    sonido.src=instrumento.audio;
+    sonido.load();
+    sonido.play();
+  }
+
+}
